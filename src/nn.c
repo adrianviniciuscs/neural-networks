18be859af45170919a3a3a369ab4fbff17eb7400
adrian <adrianvinicius.cs@gmail.com>
2024-06-07 13:56:47 -0300

    cleanup

--- src/nn.c
+++ src/nn.c
@@ -118,44 +118,37 @@ double uniform_distribution(double rangeLow, double rangeHigh)
     return rng_scaled;
 }
 
-void spiral_data(int points, int classes, spiral_data_t *data)
-{
-    data->x = (double*)malloc(sizeof(double) * points * classes * 2);
-    if (data->x == NULL) {
-        fprintf(stderr, "Error: Unable to allocate memory for data.x\n");
-        return;
-    }
+void spiral_data(int points, int classes, spiral_data_t *data) {
+    int ix = 0; // Index for x coordinates
+    int iy = 0; // Index for y coordinates
 
-    data->y = (double*)malloc(sizeof(double) * points * classes);
-    if (data->y == NULL) {
-        fprintf(stderr, "Error: Unable to allocate memory for data.y\n");
-        free(data->x);
-        return;
-    }
+    // Seed the random number generator
+    srand(time(NULL));
 
-    int ix = 0;
-    int iy = 0;
-
+    // Loop through each class to generate spiral data
     for (int class_number = 0; class_number < classes; class_number++) {
-        double r = 0;
-        double t = class_number * 4;
+        double r = 0; // Radius
+        double t = class_number * 4; // Angle
 
+        // Generate points on the spiral for the current class
         while (r <= 1 && t <= (class_number + 1) * 4) {
-            // Adding some randomness to t
+            // Add some randomness to t to introduce variation
             double random_t = t + uniform_distribution(-1.0, 1.0) * 0.2;
 
-            // Converting from polar to Cartesian coordinates
-            data->x[ix] = r * sin(random_t * 2.5);
-            data->x[ix + 1] = r * cos(random_t * 2.5);
+            // Convert polar coordinates to Cartesian coordinates
+            data->x[ix] = r * sin(random_t * 2.5); // x = r * sin(theta)
+            data->x[ix + 1] = r * cos(random_t * 2.5); // y = r * cos(theta)
 
+            // Assign the class number as the label
             data->y[iy] = class_number;
 
-            // The below two statements achieve linspace-like functionality
-            r += 1.0 / (points - 1);
-            t += 4.0 / (points - 1);
+            // Increase r and t to move to the next point on the spiral
+            r += 1.0 / (points - 1); // Increment r to move along the radius
+            t += 4.0 / (points - 1); // Increment t to move along the angle
 
-            iy++;
-            ix += 2; // increment index
+            // Increment indices to move to the next set of coordinates
+            iy++; // Increment y index
+            ix += 2; // Increment x index (2 elements per x,y pair)
         }
     }
 }
